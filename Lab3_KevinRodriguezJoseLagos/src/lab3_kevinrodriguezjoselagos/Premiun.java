22f4ae70a4fd94f3dbb26b935b2b1f37a571d2dd
kevin09102001 <kevin31344@unitec.edu>
2021-02-05 13:55:29 -0600

    mas clases

---
+++ Lab3_KevinRodriguezJoseLagos/src/lab3_kevinrodriguezjoselagos/Premiun.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_kevinrodriguezjoselagos;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Premiun extends marcas {
+
+    protected String nombre_de_su_división_deportiva;
+    protected String nombre_de_su_división_de_lujo;
+    protected String tipo;
+
+    public Premiun() {
+    }
+
+    public Premiun(String nombre, String lema, int año_de_fundacion, int año_que_seintegro, String Fundador, int total_ventas, String Presidente, int num_modelos) {
+        super(nombre, lema, año_de_fundacion, año_que_seintegro, Fundador, total_ventas, Presidente, num_modelos);
+    }
+
+    public Premiun(String nombre_de_su_división_deportiva, String nombre_de_su_división_de_lujo, String tipo, String nombre, String lema, int año_de_fundacion, int año_que_seintegro, String Fundador, int total_ventas, String Presidente, int num_modelos) {
+        super(nombre, lema, año_de_fundacion, año_que_seintegro, Fundador, total_ventas, Presidente, num_modelos);
+        this.nombre_de_su_división_deportiva = nombre_de_su_división_deportiva;
+        this.nombre_de_su_división_de_lujo = nombre_de_su_división_de_lujo;
+        this.tipo = tipo;
+    }
+
+    public String getNombre_de_su_división_deportiva() {
+        return nombre_de_su_división_deportiva;
+    }
+
+    public void setNombre_de_su_división_deportiva(String nombre_de_su_división_deportiva) {
+        this.nombre_de_su_división_deportiva = nombre_de_su_división_deportiva;
+    }
+
+    public String getNombre_de_su_división_de_lujo() {
+        return nombre_de_su_división_de_lujo;
+    }
+
+    public void setNombre_de_su_división_de_lujo(String nombre_de_su_división_de_lujo) {
+        this.nombre_de_su_división_de_lujo = nombre_de_su_división_de_lujo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
--- Lab3_KevinRodriguezJoseLagos/src/lab3_kevinrodriguezjoselagos/marcas.java
+++ Lab3_KevinRodriguezJoseLagos/src/lab3_kevinrodriguezjoselagos/marcas.java
@@ -10,5 +10,92 @@ package lab3_kevinrodriguezjoselagos;
  * @author Kevin
  */
 public class marcas {
+    protected String nombre;
+    protected String lema;
+    protected int año_de_fundacion;
+    protected int año_que_seintegro;
+    protected String Fundador;
+    protected int total_ventas;
+    protected String Presidente;
+    protected int num_modelos;
+
+    public marcas() {
+    }
+
+    public marcas(String nombre, String lema, int año_de_fundacion, int año_que_seintegro, String Fundador, int total_ventas, String Presidente, int num_modelos) {
+        this.nombre = nombre;
+        this.lema = lema;
+        this.año_de_fundacion = año_de_fundacion;
+        this.año_que_seintegro = año_que_seintegro;
+        this.Fundador = Fundador;
+        this.total_ventas = total_ventas;
+        this.Presidente = Presidente;
+        this.num_modelos = num_modelos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLema() {
+        return lema;
+    }
+
+    public void setLema(String lema) {
+        this.lema = lema;
+    }
+
+    public int getAño_de_fundacion() {
+        return año_de_fundacion;
+    }
+
+    public void setAño_de_fundacion(int año_de_fundacion) {
+        this.año_de_fundacion = año_de_fundacion;
+    }
+
+    public int getAño_que_seintegro() {
+        return año_que_seintegro;
+    }
+
+    public void setAño_que_seintegro(int año_que_seintegro) {
+        this.año_que_seintegro = año_que_seintegro;
+    }
+
+    public String getFundador() {
+        return Fundador;
+    }
+
+    public void setFundador(String Fundador) {
+        this.Fundador = Fundador;
+    }
+
+    public int getTotal_ventas() {
+        return total_ventas;
+    }
+
+    public void setTotal_ventas(int total_ventas) {
+        this.total_ventas = total_ventas;
+    }
+
+    public String getPresidente() {
+        return Presidente;
+    }
+
+    public void setPresidente(String Presidente) {
+        this.Presidente = Presidente;
+    }
+
+    public int getNum_modelos() {
+        return num_modelos;
+    }
+
+    public void setNum_modelos(int num_modelos) {
+        this.num_modelos = num_modelos;
+    }
+    
     
 }
